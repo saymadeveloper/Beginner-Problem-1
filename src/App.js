3d7bb1cb57d3b389b534c04b2345dbd716e918d5
sayma <sama@newgen-bd.com>
2022-12-11 16:27:12 +0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './component/Navbar';
+import Articles from './component/Articles';
+import Content from './component/Content';
+class App extends Component {
+  render() {
+    return (
+      <div className='app-class'>
+        <Navbar />
+        <Articles />
+        <Content />
+      </div>
+    );
+  }
+}
+
+export default App;
